019b11f5a2b347f7180fbd51e18136198f0047dd
tusharpatil1998 <158586947+tusharpatil1998@users.noreply.github.com>
2024-02-13 08:50:59 +0530

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+ef calculate_factorial(n):
+    """Calculate the factorial of a given number."""
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
